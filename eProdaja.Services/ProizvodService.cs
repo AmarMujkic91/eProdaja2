307295b7e79d17bebbde8bb4c364235f8b6b35e6
User <User@DESKTOP-S86QD93>
2024-04-13 01:51:49 +0200

    Add project files.

---
+++ eProdaja.Services/ProizvodService.cs
@@ -0,0 +1,26 @@
+﻿using eProdaja.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodService : IProizvodiService
+    {
+        List<Proizvodi> newList = new List<Proizvodi>()
+        {
+            new Proizvodi()
+            {
+                ProizvodId=1,
+                Naziv="Laptopi"
+            }
+        };
+
+        public IList<Proizvodi> Get()
+        {
+           return newList;
+        }
+    }
+}
